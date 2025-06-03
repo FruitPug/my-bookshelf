6e2dbcee1ccd5ea37855adc3cd33e46db6a8e19e
Roman Pekur <roman.pekur@gmail.com>
2025-06-03 13:26:40 +0300

    removed redundant cache map

--- src/main/java/com/example/MyBookshelf/config/CacheConfig.java
+++ src/main/java/com/example/MyBookshelf/config/CacheConfig.java
@@ -9,6 +9,6 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("recommendations", "genreSuggestions");
+        return new ConcurrentMapCacheManager("recommendations");
     }
 }
